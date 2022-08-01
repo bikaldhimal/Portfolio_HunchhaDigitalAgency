e122e76b731d31b1c8c2f82ce721043c3af62463
“Bikal <bikaldhimal2016@gmail.com>
2022-08-01 16:47:11 +0545

    Deploy in Heroku

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Map from "./components/map/Map";
 import Footer from "./components/footer/Footer";
 import Testimonials from "./components/testimonials/Testimonials";
 import Nav from "./components/nav/Nav";
+import Messenger from "./components/messenger/Messenger";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Map />
       <Testimonials />
       <Footer />
+      <Messenger />
     </>
   );
 };
---
+++ src/components/messenger/Messenger.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import MessengerCustomerChat from "react-messenger-customer-chat";
+
+const Messenger = () => {
+  return (
+    <>
+      <MessengerCustomerChat pageId="898639190481373" appId="354318836906983" />
+    </>
+  );
+};
+
+export default Messenger;
